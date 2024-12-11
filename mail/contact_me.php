512922f406a2befee18928ac970298c005702d80
Amaan6868 <123187335+Amaan6868@users.noreply.github.com>
2024-12-10 20:05:07 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -25,7 +25,7 @@ if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) 
     $to = 'amaanafzalcs@gmail.com'; 
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-    $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers = "From: noreply@Amaan6868.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
     $headers .= "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
     return true;
